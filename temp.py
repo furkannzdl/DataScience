97567ab00b8995ffe25fae0d70a090c935f9061f
ozdaldw <furkanozdal27@gmail.com>
2024-09-06 02:36:32 +0300

    progress with svm

--- temp.py
+++ temp.py
@@ -1,11 +1,12 @@
-class Solution:
-    def maxArea(self, height: list[int]) -> int:
-        m = 1
-        for i in range(len(height)):
-            for j in range(len(height)):
-                m = max(min(height[i],height[j]) * abs(j-i),m)
-        return m
-    
 
-s = Solution()
-print(s.maxArea([1,8,6,2,5,4,8,3,7]))
+
+
+arr = [1,8,6,2,5,4,8,3,7,7,7,7,8,8,8,1]
+
+arr = set(arr)
+if 5 in arr:
+    print("yes")
+
+for i in arr:
+    print("ha")
+print(9//4)
