89ef02df57d7d3e90b8861145e62c46c06d503cf
ozdaldw <furkanozdal27@gmail.com>
2024-09-13 22:28:03 +0300

    new ml algorithm committed

--- temp.py
+++ temp.py
@@ -1,4 +1,4 @@
-
+from sklearn.model_selection import train_test_split
 
 
 arr = [1,8,6,2,5,4,8,3,7,7,7,7,8,8,8,1]
@@ -10,3 +10,6 @@ if 5 in arr:
 for i in arr:
     print("ha")
 print(9//4)
+def fun(x,y,z):
+    return x*y*z
+print(fun(3,4,5))
